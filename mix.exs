1608edba2542e61e4d6b8628d3e0ff7e4e2fa545
Robert Boone <robert@rlb3.com>
2018-04-26 16:41:59 -0500

    Add comeonin

--- mix.exs
+++ mix.exs
@@ -39,7 +39,9 @@ defmodule VsaDriver.Mixfile do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:ja_serializer, "~> 0.13.0"}
+      {:ja_serializer, "~> 0.13.0"},
+      {:comeonin, "~> 4.0"},
+      {:argon2_elixir, "~> 1.2"}
     ]
   end
 
