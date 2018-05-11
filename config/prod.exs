841b170297364977dab1f76574c7eedfbec9e850
Robert Boone <robert@rlb3.com>
2018-05-11 09:24:50 -0500

    SendGrid Config

--- config/prod.exs
+++ config/prod.exs
@@ -70,3 +70,7 @@ config :vsa_driver, VsaDriver.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
+
+config :vsa_driver, VsaDriver,
+  adapter: Swoosh.Adapters.Sendgrid,
+  api_key: System.get_env("SENDGRID_API") || ""
